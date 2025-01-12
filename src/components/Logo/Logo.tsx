837df2683481cbe1f28240784cbc9ec5134bc13e
abesandala <abesandala@gmail.com>
2025-01-12 05:24:36 +0200

    fonts added

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -17,13 +17,13 @@ export const Logo = (props: Props) => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Payload Logo"
-      width={193}
-      height={34}
+      width={220}
+      height={60}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      className={clsx('max-w-[13rem] w-full h-[80px]', className)}
+      src="./logo_white.svg"
     />
   )
 }
